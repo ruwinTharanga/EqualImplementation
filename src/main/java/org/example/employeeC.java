d00033ff6c1809975a863848b691f209bc4e165b
ruwinTharanga <ruwintharanga@gmail.com>
2022-08-19 12:10:24 +0530

    q4

---
+++ src/main/java/org/example/employeeC.java
@@ -0,0 +1,70 @@
+package org.example;
+
+public class employeeC {
+
+    private int id;
+    private int code;
+    private String name;
+    private int nic;
+    private String address;
+
+    public employeeC(int id, int code, String name, int nic, String address) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.nic = nic;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNic() {
+        return nic;
+    }
+
+    public void setNic(int nic) {
+        this.nic = nic;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        employeeC employee = (employeeC) obj;
+        if(this == obj){
+            return true;
+        } else if (!(obj instanceof employeeC)) {
+            return false;
+        }
+        return getId() == getId() && getCode() == getCode();
+       // getCode().equals(getCode());
+    }
+}
